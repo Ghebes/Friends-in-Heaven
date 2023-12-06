03fd2cd52194f396a465daf27e195774c438a44c
Michael Ghebranious <ghebraniousmike@gmail.com>
2023-12-06 15:10:01 -0600

    Initial Commit

---
+++ Friends in Heaven/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friends in Heaven
+//
+//  Created by Michael Ghebranious on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friends in Heaven/Friends_in_HeavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friends_in_HeavenApp.swift
+//  Friends in Heaven
+//
+//  Created by Michael Ghebranious on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Friends_in_HeavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
