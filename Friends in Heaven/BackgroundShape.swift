c67a1bcb85d25287a5976b500e307d659cd51ec7
Michael Ghebranious <ghebraniousmike@gmail.com>
2023-12-08 16:45:16 -0600

    Header Background

---
+++ Friends in Heaven/BackgroundShape.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundShape.swift
+//  Friends in Heaven
+//
+//  Created by Michael Ghebranious on 12/7/23.
+//
+
+import SwiftUI
+
+struct BackgroundShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundShape()
+}
